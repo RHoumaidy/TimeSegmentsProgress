486d5c6b049a2473c30d65c94447527f12e18f28
Raafat Alhmidi <raafat.alhmidi@planet1world.com>
2020-07-09 21:57:07 +0800

    V1.0.2

--- timeSegment/src/main/java/com/raafat/timesegment/TimeSegmentView.kt
+++ timeSegment/src/main/java/com/raafat/timesegment/TimeSegmentView.kt
@@ -1,10 +1,7 @@
 package com.raafat.timesegment
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.ColorInt
@@ -12,6 +9,7 @@ import androidx.core.content.withStyledAttributes
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.min
 
 /**
  * Created by Raafat Alhmidi on 7/9/20 @4:33 PM.
@@ -21,6 +19,10 @@ class TimeSegmentView @JvmOverloads constructor(
     attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private val sdf = SimpleDateFormat("h a", Locale.US)
+    private val sdf2 = SimpleDateFormat("hh:mm a", Locale.US)
+
+    private var path = Path()
+    private val bounds = Rect()
 
     private var backgroundPaint: Paint = Paint().apply {
         isAntiAlias = true
@@ -34,22 +36,20 @@ class TimeSegmentView @JvmOverloads constructor(
         color = Color.BLACK
         style = Paint.Style.FILL
     }
-
-    init {
-        sdf.timeZone = TimeZone.getTimeZone("GMT")
-
-        context.withStyledAttributes(attrs, R.styleable.TimeSegmentView) {
-            cornersRadius = getDimension(R.styleable.TimeSegmentView_corners_radius, 0.0f)
-            strokeWidth = getDimension(R.styleable.TimeSegmentView_stroke_width, 1f)
-            background_color = getColor(R.styleable.TimeSegmentView_background_color, Color.WHITE)
+    var times: ArrayList<TimeItem> = ArrayList()
+        set(value) {
+            field = value
+            invalidate()
         }
-
-    }
-
 
     private var cornersRadius = 0.0f
         set(value) {
             field = value
+            invalidate()
+        }
+    private var segmentCornersRadius = 0.0f
+        set(value) {
+            field = min(value,cornersRadius)
             invalidate()
         }
     private var strokeWidth = 1.0f
@@ -69,26 +69,51 @@ class TimeSegmentView @JvmOverloads constructor(
             invalidate()
         }
 
+    private var textHeight = 0
 
-    var times: ArrayList<TimeItem> = ArrayList()
-        set(value) {
-            field = value
-            invalidate()
+
+
+    init {
+        sdf.timeZone = TimeZone.getTimeZone("GMT")
+        sdf2.timeZone = TimeZone.getTimeZone("GMT")
+
+        context.withStyledAttributes(attrs, R.styleable.TimeSegmentView) {
+            cornersRadius = getDimension(R.styleable.TimeSegmentView_corners_radius, 0.0f)
+            strokeWidth = getDimension(R.styleable.TimeSegmentView_stroke_width, 1f)
+            background_color = getColor(R.styleable.TimeSegmentView_background_color, Color.WHITE)
+            segmentCornersRadius =
+                getDimension(R.styleable.TimeSegmentView_segment_corners_radius, 0.0f)
         }
+
+        textPaint.getTextBounds("12 AM", 0, "12 AM".length, bounds)
+        textHeight = bounds.height() + 5
+        path.reset()
+        path.addRoundRect(
+            0f,
+            0f,
+            width.toFloat(),
+            height.toFloat() - textHeight,
+            cornersRadius,
+            cornersRadius,
+            Path.Direction.CW
+        )
+        path.close()
+
+    }
 
 
     private var labels: ArrayList<Long> = arrayListOf(
-        sdf.parse("2 AM")?.time?:0,
-        sdf.parse("4 AM")?.time?:0,
-        sdf.parse("6 AM")?.time?:0,
-        sdf.parse("8 AM")?.time?:0,
-        sdf.parse("10 AM")?.time?:0,
-        sdf.parse("12 PM")?.time?:0,
-        sdf.parse("2 PM")?.time?:0,
-        sdf.parse("4 PM")?.time?:0,
-        sdf.parse("6 PM")?.time?:0,
-        sdf.parse("8 PM")?.time?:0,
-        sdf.parse("10 PM")?.time?:0
+        sdf.parse("2 AM")?.time ?: 0,
+        sdf.parse("4 AM")?.time ?: 0,
+        sdf.parse("6 AM")?.time ?: 0,
+        sdf.parse("8 AM")?.time ?: 0,
+        sdf.parse("10 AM")?.time ?: 0,
+        sdf.parse("12 PM")?.time ?: 0,
+        sdf.parse("2 PM")?.time ?: 0,
+        sdf.parse("4 PM")?.time ?: 0,
+        sdf.parse("6 PM")?.time ?: 0,
+        sdf.parse("8 PM")?.time ?: 0,
+        sdf.parse("10 PM")?.time ?: 0
 
     )
 
@@ -97,35 +122,50 @@ class TimeSegmentView @JvmOverloads constructor(
         invalidate()
     }
 
-    fun addItem(start:Long, end:Long,@ColorInt color:Int){
-        addItem(TimeItem(start,end,color))
+    fun addItem(start: Long, end: Long, @ColorInt color: Int) {
+        addItem(TimeItem(start, end, color))
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
+        textPaint.getTextBounds("12 AM", 0, "12 AM".length, bounds)
+        textHeight = bounds.height() + 5
+        path.reset()
+        path.addRoundRect(
+            0f,
+            0f,
+            width.toFloat(),
+            height.toFloat() - textHeight,
+            cornersRadius,
+            cornersRadius,
+            Path.Direction.CW
+        )
+        path.close()
+
         invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
-        val bounds = Rect()
-        textPaint.getTextBounds("12 AM", 0, "12 AM".length, bounds)
-        val textHeight = bounds.height() + 5
 
         canvas?.apply {
 
             for (label in labels) {
                 val text = sdf.format(label)
                 textPaint.getTextBounds(text, 0, text.length, bounds)
-                drawText(text, label.toFloat()/(24*60*60*1000)*width - bounds.width() / 2, height.toFloat(), textPaint)
+                drawText(
+                    text,
+                    label.toFloat() / (24 * 60 * 60 * 1000) * width - bounds.width() / 2,
+                    height.toFloat(),
+                    textPaint
+                )
             }
+            clipPath(path)
 
-            drawRoundRect(
+            drawRect(
                 0f,
                 0f,
                 width.toFloat(),
                 height.toFloat() - textHeight,
-                cornersRadius,
-                cornersRadius,
                 backgroundPaint
             )
 
@@ -135,12 +175,11 @@ class TimeSegmentView @JvmOverloads constructor(
                     0f,
                     item.getEndX() * width,
                     height.toFloat() - textHeight,
-                    cornersRadius,
-                    cornersRadius,
+                    segmentCornersRadius,
+                    segmentCornersRadius,
                     item.getPaint()
                 )
             }
-
 
         }
         super.onDraw(canvas)
